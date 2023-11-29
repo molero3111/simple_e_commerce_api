8cf5cc823ef334a82b0aaca60f48fc133128d8dc
Emmanuel Molero <37030461+molero3111@users.noreply.github.com>
2023-11-29 11:02:32 -0300

    Removed commet in api.php

--- routes/api.php
+++ routes/api.php
@@ -32,8 +32,3 @@ Route::group(['middleware'=>['auth:sanctum']], function (){
      Route::post('/logout', [AuthController::class, 'logout']);
     
  });
-
-
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
