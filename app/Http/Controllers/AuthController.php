caba76bc4ffcb2a7a065303bc9dd8f11e4a87ad5
Emmanuel Molero <37030461+molero3111@users.noreply.github.com>
2023-11-29 11:01:30 -0300

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,28 +19,20 @@ class AuthController extends Controller
         if(!$result){
             return ['type'=>'error', 'message'=>'User could not be created'];
         }
-
         $response = [
             'type'=>'success',
             'message'=>'User registered successfully',
             'user' => $userDAO->entity,
             'token' => $userDAO->token
         ];
-
-        $result = $userDAO = null;
-
         return response($response, 201);
     }
 
     public function login(LoginRequest $request)
     {
-        // return $request;
-        // return $request->all();
         $fields = $request->validated();
-        // $fields = $request->all();
         //get user by EMAIL
         $user = User::where('email', $fields['email'])->first();
-
         if (!Hash::check($fields['password'], $user->password)) {
             return response(
                 [
@@ -51,18 +43,15 @@ class AuthController extends Controller
                 ]
             );
         }
-
         $response = [
             'user' => $user,
             'token' => $user->createToken('user_token_' . $user->id)->plainTextToken
         ];
-
         return response($response, 201);
     }
 
     public function logout(Request $request)
     {
-
         $request->user()->tokens()->delete();
         return ['type' => 'success', 'message' => 'Logged out succesfully'];
     }
